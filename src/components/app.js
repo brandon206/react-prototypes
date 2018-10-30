86c9cc20c143bcd2f7886b06b44be60752234127
Brandon Park <brandonpark206@gmail.com>
2018-10-30 10:52:00 -0700

    adding functional components in react

--- src/components/app.js
+++ src/components/app.js
@@ -1,2 +1,27 @@
+import React from 'react';
+import Table from './table';
 
+const students = [{
+    student:'Brandon',
+    course:'Math',
+    grade: 80
+},
+{
+    student:'Vienna',
+    course:'Science',
+    grade: 100
+},
+{
+    student:'Kyle',
+    course:'Surfing',
+    grade: 100
+}];
 
+export default (data) => {
+    return(
+        <div className = 'container'>
+            <h1>Student Grade Table</h1>
+            <Table data = {students}/>
+        </div>
+    )
+}
