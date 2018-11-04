13e7f7b5f0d6534138afc1d513d0a1a8a1b394a7
Brandon Park <brandonpark206@gmail.com>
2018-11-03 22:32:41 -0700

    completed input prototype

---
+++ src/data/contacts.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        firstName: 'Kyle',
+        lastName: 'Umbarger',
+        phone: '949-949-9494',
+        email: 'kyleumbarger@gmail.com'
+    },
+    {
+        firstName: 'Andy',
+        lastName: 'Park',
+        phone: '213-213-2132',
+        email: 'andypark@outlook.com' 
+    },
+    {
+        firstName: 'Tiffany',
+        lastName: 'Kim',
+        phone: '562-562-562',
+        email: 'tiffanykim@hotmail.com'
+    },
+    {
+        firstName: 'Vienna',
+        lastName: 'Tran',
+        phone: '626-626-6262',
+        email: 'viennatran@yahoo.com'
+    }
+];
