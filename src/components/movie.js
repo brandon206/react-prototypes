c64f9fc9b860ce38e0b4f61beba1599a157e1157
Brandon Park <brandonpark206@gmail.com>
2018-11-09 14:41:29 -0800

    added additional css

--- src/components/movie.js
+++ src/components/movie.js
@@ -3,8 +3,8 @@ import React, {Fragment} from 'react';
 const Movie = (props) => {
     const {info} = props;
     return (
-        <div>
-            <div className = "container">
+        <div className = "container">
+            <div className = "section">
                 <img src= {info['im:image'][2].label} alt=""/>
                 <h3> {info['im:name'].label} </h3>
                 <p> {info.summary.label} </p>
