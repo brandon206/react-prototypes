256521eb6e0b8532179525e11dedc612578138cc
Brandon Park <brandonpark206@gmail.com>
2018-11-09 13:05:57 -0800

    added materialize css

--- src/components/movie.js
+++ src/components/movie.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {Fragment} from 'react';
 
 const Movie = (props) => {
     const {info} = props;
     return (
         <div>
-            <img src= {info['im:image'][2].label} alt=""/>
-            <h3> {info['im:name'].label} </h3>
-            <p> {info.summary.label} </p>
+            <div className = "container">
+                <img src= {info['im:image'][2].label} alt=""/>
+                <h3> {info['im:name'].label} </h3>
+                <p> {info.summary.label} </p>
+            </div>
+            <div className = "divider"></div>
         </div>
-    )
+    );
 }
 
 export default Movie;
